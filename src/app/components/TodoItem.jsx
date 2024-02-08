e06f49bf6b107c3c4c0c0bef74d72a80efed4a7e
japhethbear <japhethbear@gmail.com>
2024-02-08 12:04:24 -0500

    first commit

---
+++ src/app/components/TodoItem.jsx
@@ -0,0 +1,15 @@
+"use client"
+
+function TodoItem({ id, title, complete, toggleTodo}) {
+
+    return (
+    <li className="flex gap-1 items-center">
+        <input id={id} type="checkbox" className="cursor-pointer peer"
+        defaultChecked={complete}
+        onChange={e => toggleTodo(id, e.target.checked)}/>
+        <label htmlFor={id} className="cursor-pointer peer-checked:line-through peer-checked:text-slate-500">{title}</label>
+    </li>
+    )
+}
+
+module.exports = TodoItem;
